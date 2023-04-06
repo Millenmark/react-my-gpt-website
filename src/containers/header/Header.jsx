8795e7efa9e0e1dc0a1f7850469f63086c372b2b
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-06 14:15:20 +0800

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,27 @@
 import './HeaderStyles.css'
+import people from '../../assets/images/people.png'
+import ai from '../../assets/images/ai.png'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's build something amazing with GPT-3 OpenAI
+        </h1>
+        <p>ChatGPT3: the ultimate solution for seamless customer engagement. Our cutting-edge technology understands natural language and responds in real-time, providing personalized attention instantly. Say goodbye to long wait times and hello to instant gratification. Try ChatGPT3 today and experience the future of customer engagement!</p>
+        
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+        </div>
+      </div>
+    </div>
   )
 }
 
