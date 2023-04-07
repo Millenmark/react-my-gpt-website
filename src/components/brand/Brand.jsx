c41a8c8282801909cb6564a150d4c903e7349f33
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-07 15:30:07 +0800

    Imported the brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,21 @@
 import './BrandStyles.css'
+import * as brands from './imports'
 import React from 'react'
 
+
 const Brand = () => {
+  const brandNames = Object.keys(brands)
+
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      {
+        brandNames.map((name, index) => (
+          <div key={index}>
+            <img src={brands[name]} alt="google" />
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
